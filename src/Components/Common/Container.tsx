8900bd84a2c372e47bee33dbb759089b4dda899e
Abdullah-167 <abdullahkhizer007@gmail.com>
2025-05-30 15:53:48 +0500

    new updates

--- src/Components/Common/Container.tsx
+++ src/Components/Common/Container.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
-const Container = ({ children }: any) => {
-  return <div className="max-w-[1300px] 2xl:max-w-[1400px] mx-auto px-5">{children}</div>;
+interface ContainerProps {
+  children: ReactNode;
+  className?: string;
+}
+
+const Container: React.FC<ContainerProps> = ({ children, className = "" }) => {
+  return (
+    <div className={`max-w-[1300px] 2xl:max-w-[1400px] mx-auto px-5 ${className}`}>
+      {children}
+    </div>
+  );
 };
 
 export default Container;
